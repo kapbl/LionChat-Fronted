da9652c1c8407fac2c936598e9b09f521ca153da
capybara <tisoyim@outlook.com>
2025-08-18 16:22:51 +0800

    chore: 将API端点从远程服务器更改为本地开发环境

--- src/LoginRegister.vue
+++ src/LoginRegister.vue
@@ -40,7 +40,7 @@
     errorMsg.value = ''
     loading.value = true
     try {
-      const resp = await fetch('http://62.234.192.227/v1/api/user/login', {
+      const resp = await fetch('http://localhost/v1/api/user/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username: username.value, password: password.value })
@@ -66,7 +66,7 @@
     errorMsg.value = ''
     loading.value = true
     try {
-      const resp = await fetch('http://62.234.192.227/v1/api/user/register', {
+      const resp = await fetch('http://localhost/v1/api/user/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username: username.value, password: password.value, nickname: nickname.value })
